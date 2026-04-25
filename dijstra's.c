58321549d5a4d509183ddffe4a19783b4ba41432
11249a225-droid <11249a225@kanchiuniv.ac.in>
2026-04-25 22:08:50 +0530

    implementation of shortesrt path algorithm( dijstra's)

--- dijstra's.c
+++ dijstra's.c
@@ -1,3 +1,15 @@
+AIM:
+To implement the shortest path algoritm usind dijkstras 
+
+ALGORITHM:
+1. Initialize a dist array where dist[source] = 0 and all others are ∞. 
+2. Maintain a set of unvisited vertices. 
+3. While there are unvisited vertices: 
+o Select the vertex u with the minimum dist[u]. 
+o For each neighbor v of u:  
+▪ If dist[u] + weight(u, v) < dist[v]:  
+▪ Update dist[v] (Relaxation).
+program
 #include<stdio.h>
 #define V 5
 #define INF 999999
