13482158ce06bd2c48c9ca8f5e8960404530c249
11249a225-droid <11249a225@kanchiuniv.ac.in>
2026-04-25 22:22:40 +0530

    implementation of tower of hanoi

--- tower of hanoi.c
+++ tower of hanoi.c
@@ -1,3 +1,13 @@
+AIM:
+To implement tower of hanoi using recursion to move n disks from sourse to destination rod
+
+ALGORITHM:
+1. check the  Base Condition. 
+2. Move Top (n−1) Disks from Source to Auxiliary
+3. Move the largest disk (nth disk) to destination
+4. Move (n−1) Disks from Auxiliary  to destination
+5. Repeat Recursively until base condition satisfied.
+program
 #include <stdio.h>
  /**
  * Function to solve Tower of Hanoi problem
