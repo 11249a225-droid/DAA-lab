6b1b8152d2734fecbd5f87900e8ff89b10212f2c
11249a225-droid <11249a225@kanchiuniv.ac.in>
2026-04-25 22:10:16 +0530

    implementation of using fibonacci recursion

--- fibonacci recursion.c
+++ fibonacci recursion.c
@@ -1,3 +1,13 @@
+AIM:
+To implement a program to find the nth fibonacci term using recursion
+
+ALGORITHM:
+1.read the vaalue of n
+2.if n=0,return 0(base condition)
+3.if n=1,return 1(base condition)
+4.recursively compute: f(n)=f(n-1)+f(n-2)
+5.display the returned value as the nth fibonacci term.
+program
 #include <stdio.h>
 
  /**
