e98444a6e745df1f1d84d1e623020a95f7e8f059
11249a225-droid <11249a225@kanchiuniv.ac.in>
2026-04-25 22:18:20 +0530

    implementation of minimum spanning tree using prims algorithm

--- prims.c
+++ prims.c
@@ -1,3 +1,16 @@
+AIM:
+to implement the prims algorithm to find the minimum spanning tree of a given weighted connected graph using greedt method.
+ 
+Prim’s Algorithm 
+1. Start with any vertex  
+2. Mark it as visited  
+3. Look at all edges from visited vertices  
+4. Consider only edges going to unvisited vertices  
+5. Pick the smallest (minimum weight) edge  
+6. Add that edge and the new vertex to the tree  
+7. Repeat steps 3–6  
+8. Continue until all vertices are included
+program
 #include <stdio.h>
 #include <stdlib.h>
 #define V 5 // Number of vertices
